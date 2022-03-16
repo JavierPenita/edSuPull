aef80b9ec0f0f7d2e0430af8481a0f56483476cf
javi <javi@DESKTOP-KTATCQS>
2022-03-16 12:31:50 +0100

    Agregar archivos de proyecto.

---
+++ DATSI/SD/edsupull.2022/broker/comun.c
@@ -0,0 +1,19 @@
+/*
+ * Incluya en este fichero todas las implementaciones que pueden
+ * necesitar compartir el broker y la biblioteca, si es que las hubiera.
+ */
+#include "comun.h"
+
+#include <unistd.h>
+#include <stdio.h>
+
+// debe usarse para obtener un UUID para el cliente
+int generate_UUID(UUID_t uuid) {
+    FILE *d = popen("uuidgen", "r");
+    if (d) {
+        fread(uuid, sizeof(UUID_t), 1, d);
+	uuid[sizeof(UUID_t)]='\0';
+    }
+    pclose(d);
+    return (d? 0:-1);
+}
